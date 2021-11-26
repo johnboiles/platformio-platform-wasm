4b96b3a09be02df5219782aac0421b278ebd240d
John Boiles <johnaboiles@gmail.com>
2021-11-25 20:40:42 -0700

    Fail fast if Emscripten can't be found

--- builder/main.py
+++ builder/main.py
@@ -2,7 +2,18 @@
     Builder for web assembly
 """
 
+import subprocess
+import sys
 from SCons.Script import AlwaysBuild, Default, DefaultEnvironment
+
+try:
+    subprocess.check_output(["em++", "--version"])
+except FileNotFoundError:
+    print(
+        "Could not find emscripten. Maybe install it? (e.g. `brew install emscripten` on macOS. See also: https://emscripten.org/docs/getting_started/downloads.html)",
+        file=sys.stderr,
+    )
+    exit(1)
 
 env = DefaultEnvironment()
 
@@ -11,10 +22,10 @@ env.Append(
 )
 
 env.Replace(
-    CXX='em++',
-    CC='emcc',
-    AR='emar',
-    RANLIB='emranlib',
+    CXX="em++",
+    CC="emcc",
+    AR="emar",
+    RANLIB="emranlib",
     PROGSUFFIX=".html"
 )
 
